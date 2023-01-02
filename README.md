70382789cc3b185fde08c951fc085e9b94582349
Aryan Gupta <aryanb203@gmail.com>
2023-01-02 17:54:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Project- Library Management System
+A simple and basic use library management system which tracks books, their information, if they are issued or available and who issued the book.
+There is an admin as well as a user side.
+
+Tech/Frameworks/Tools Used:
+HTML, CSS, Bootstrap
+PHP, MySQL, XAMPP
+
+How to use:
+
+1-Install Xampp on your computer.
+
+2-Download the zip file and extract its content in a folder named htdocs in the Xampp folder.
+
+3-Start the MySQL and Apache modules from XAMPP Control Panel.
+
+4-Copy the port number of MySQL module from XAMPP Control Panel and then paste it in database.php file instead of 8111(i.e. my port number).
+
+5-Go to google chrome or any web browser and type localhost on it.
+
+6-Select phpMyAdmin.
+
+7-Create a new database and name it as "library-system".(same as the sql file included in the project)
+
+8-Import "library-system.sql" file from the system and execute the queries.
+
+9-Type localhost/DBMS_Proj on your browser and now the website should be working.
---
+++ database sql file/library-system.sql
@@ -0,0 +1,249 @@
+
+CREATE TABLE `author` (
+  `authorid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`authorid`, `name`, `status`) VALUES
+(2, 'SampleAuthor1', 'Enable'),
+(3, 'SampleAuthor2', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `bookid` int(11) NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `rackid` int(11) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `picture` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `publisherid` int(11) NOT NULL,
+  `isbn` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `no_of_copy` int(5) NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bookid`, `categoryid`, `authorid`, `rackid`, `name`, `picture`, `publisherid`, `isbn`, `no_of_copy`, `status`) VALUES
+(1, 2, 2, 2, 'DBMS', 'DBMS-cover.jpg', 8, 'B00BALXN70', 10, 'Enable'),
+(2, 2, 3, 2, 'Goosebumps', 'Goosebumps-cover.jpg', 9, '0596006306', 10, 'Enable'),
+(3, 2, 2, 1, 'NewBook', '', 7, 'sdfsd2334', 23, 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryid`, `name`, `status`) VALUES
+(1, 'Fiction', 'Enable'),
+(2, 'Non Fiction', 'Enable'),
+(3, 'Fantasy', 'Enable'),
+(4, 'Math', 'Enable'),
+(6, 'Science', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_book`
+--
+
+CREATE TABLE `issued_book` (
+  `issuebookid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `issue_date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `expected_return_date` datetime NOT NULL,
+  `return_date_time` datetime NOT NULL,
+  `status` enum('Issued','Returned','Not Return') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `issued_book`
+--
+
+INSERT INTO `issued_book` (`issuebookid`, `bookid`, `userid`, `issue_date_time`, `expected_return_date`, `return_date_time`, `status`) VALUES
+(1, 2, 2, '2022-06-12 15:33:45', '2022-06-15 16:27:59', '2022-06-16 16:27:59', 'Issued'),
+(3, 3, 3, '2022-06-12 18:46:07', '2022-06-30 18:46:02', '2022-06-12 18:46:14', 'Returned');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `publisherid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` enum('Enable','Disable') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`publisherid`, `name`, `status`) VALUES
+(2, 'Sample Publisher1', 'Enable'),
+(3, 'Publisher 2', 'Enable'),
+(4, 'Vintage Publishing', 'Disable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rack`
+--
+
+CREATE TABLE `rack` (
+  `rackid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Enable'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `rack`
+--
+
+INSERT INTO `rack` (`rackid`, `name`, `status`) VALUES
+(1, '1', 'Enable'),
+(2, '2', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
+(1, 'Tanmay', 'Gautam', 'tanmay@gmail.com', '202cb962ac59075b964b07152d234b70', 'user'),
+(2, 'Aryan', NULL, 'aryan@gmail.com', '202cb962ac59075b964b07152d234b70', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`authorid`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`bookid`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryid`);
+
+--
+-- Indexes for table `issued_book`
+--
+ALTER TABLE `issued_book`
+  ADD PRIMARY KEY (`issuebookid`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`publisherid`);
+
+--
+-- Indexes for table `rack`
+--
+ALTER TABLE `rack`
+  ADD PRIMARY KEY (`rackid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `authorid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `issued_book`
+--
+ALTER TABLE `issued_book`
+  MODIFY `issuebookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `publisherid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `rack`
+--
+ALTER TABLE `rack`
+  MODIFY `rackid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
